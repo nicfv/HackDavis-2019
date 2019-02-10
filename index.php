3e54f08e6cce684f6318931f2404275b5f49c683
nicfv <omg.my.junkmail@gmail.com>
2019-02-10 03:14:26 -0800

    Sad attempt at routing

--- index.php
+++ index.php
@@ -24,9 +24,13 @@
         <div id="footer"><p>Each point on the map is someone experiencing a medical emergency who will be receiving help immediately.</p></div>
     </body>
     <script>
+        // Create variables.
+        var map;
+        var directionsService;
         // Create an empty array to use as markers on the map.
         var patients = [];
         var markers = [];
+        var j;
         // Draw the map in the div element centered on Davis, CA.
         function initMap() {
             // Create a directions service.
@@ -37,8 +41,11 @@
                 gestureHandling: "cooperative",
                 streetViewControl: false
             });
+            // Create a places service.
+            var placesService = new google.maps.places.PlacesService(map);
             // Draw each marker on the map.
             for(var i=0; i<patients.length; i++) {
+                j = i;
                 markers.push(new google.maps.Marker({
                     position: {lat: patients[i].lat, lng: patients[i].lon},
                     map: map,
@@ -51,15 +58,30 @@
                     var id = patients[markers.length-1].id;
                     markers[markers.length-1].addListener("click", function() { window.location.href = "/Patient?id="+id; }, false);
                 }()); // Immediate invocation.
-                // Draw a new direction.
+                // Search for the nearest hospital and draw a new direction.
                 (function () {
+                    // Search for the nearest hospital.
+                    placesService.nearbySearch({
+                        location: {lat: patients[i].lat, lng: patients[i].lon},
+                        type: "hospital",
+                        rankBy: google.maps.places.RankBy.DISTANCE
+                    }, finishsearch);
+                }()); // Immediate invocation.
+            }
+        }
+        
+        function finishsearch(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                if(results.length > 0) {
+                    var nearest = results[0].geometry.location;
+                    // Draw a new route.
                     var directionsDisplay = new google.maps.DirectionsRenderer({
                         map: map,
                         markerOptions: {visible: false}
                     });
                     var request = {
-                        origin: {lat: patients[i].lat, lng: patients[i].lon},
-                        destination: {lat: 38.54273, lng: -121.76204},
+                        origin: {lat: patients[j].lat, lng: patients[j].lon},
+                        destination: nearest,
                         travelMode: 'DRIVING'
                     };
                     directionsService.route(request, function(result, status) {
@@ -67,8 +89,11 @@
                             directionsDisplay.setDirections(result);
                         }
                     });
-                }()); // Immediate invocation.
+                    return;
+                }
+                console.error("No locations found.");
             }
+            console.error("Error in Google Places API.");
         }
 
         function patient(id, name, lat, lon) {
@@ -89,5 +114,5 @@
         echo 'addPatients();';
         echo '</script>';
     ?>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBw8BY4jfE9IdMdfYwoYOd8KiNN4Fexf9Q&callback=initMap"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBw8BY4jfE9IdMdfYwoYOd8KiNN4Fexf9Q&libraries=places&callback=initMap"></script>
 </html>
